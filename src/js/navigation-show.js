c3cf95c039a20c58b776fff3e61f991fb570af99
Christian Rafael <crguado@gmail.com>
2020-10-11 01:48:34 -0500

    Fix: fis errors on navbar

--- src/js/navigation-show.js
+++ src/js/navigation-show.js
@@ -1,8 +1,15 @@
-const hamburguer = document.querySelector(".navigation__button");
-const navigation = document.querySelector(".navigation__background");
+window.onload = () => {
+  const hamburguer = document.querySelector(".navigation__button");
+  const navigation = document.querySelector(".navigation__background");
+  const hamburguerClose = document.querySelector(
+    ".navigation__hamburger-close"
+  );
+  const hamburguerOpen = document.querySelector(".navigation__hamburger");
 
-function showList() {
-  navigation.classList.toggle("active");
-}
-
-hamburguer.addEventListener("click", showList);
+  function showList() {
+    navigation.classList.toggle("active");
+    hamburguerClose.classList.toggle("active");
+    hamburguerOpen.classList.toggle("hidde");
+  }
+  hamburguer.addEventListener("click", showList);
+};

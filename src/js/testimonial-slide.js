dc6b9f4eb1f690e8d768e8bae1f3d1f466eb2fca
Christian Rafael <crguado@gmail.com>
2020-11-07 01:47:24 -0500

    Feat: Testimonial section finished using swiper library

--- src/js/testimonial-slide.js
+++ src/js/testimonial-slide.js
@@ -1,8 +1,45 @@
 // core version + navigation, pagination modules:
 import Swiper, { Navigation, Pagination } from "swiper";
 
+// import Swiper styles
+import "swiper/swiper-bundle.css";
+import "../styles/partials/testimonial.css";
+
 // configure Swiper to use modules
 Swiper.use([Navigation, Pagination]);
 
 // init Swiper:
-const swiper = new Swiper(".swiper-container");
+const swiper = new Swiper(".swiper-container", {
+  pagination: {
+    el: ".swiper-pagination",
+    bulletClass: "bullet",
+    bulletActiveClass: "bullet-color",
+  },
+  paginationClickable: true,
+  slidesPerView: 4,
+  allowTouchMove: true,
+  spaceBetween: 50,
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    //   640: {
+    //     slidesPerView: 2,
+    //     spaceBetween: 20,
+    //   },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+  },
+});

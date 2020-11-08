017f9ba833359113f02ad5d238785c52a108ed2a
Manuel Garcia <manueldev250799@gmail.com>
2020-11-08 14:12:59 -0500

    feat: fix onload method

--- src/js/email-form.js
+++ src/js/email-form.js
@@ -1,15 +1,13 @@
-window.onload = () => {
-  const form = document.querySelector(".site-footer__form");
-  const inputForm = document.querySelector(".site-footer__form-input");
+const form = document.querySelector(".site-footer__form");
+const inputForm = document.querySelector(".site-footer__form-input");
 
-  form.addEventListener("submit", (evt) => {
-    evt.preventDefault();
-    const inputValue = inputForm.value;
-    const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-    if (!emailRegex.test(inputValue)) {
-      inputForm.classList.add("error");
-    } else {
-      inputForm.classList.remove("error");
-    }
-  });
-};
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const inputValue = inputForm.value;
+  const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  if (!emailRegex.test(inputValue)) {
+    inputForm.classList.add("error");
+  } else {
+    inputForm.classList.remove("error");
+  }
+});
